426c78e558ab32b8622a9ef3bc25ac3ebd75274a
bsmth <brian.smith@emnify.com>
2019-10-08 10:41:48 +0200

    add headers logging

--- index.js
+++ index.js
@@ -22,18 +22,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .post('/ussd', (req, res) => {
-    console.log(req)
-    console.log(req.body)
-    res.status(200).send({
-      "ussd-continue": {
-        "message": {
-          "encoding": "default",
-          "body": "looks good!"
-        }
-      }
-    })
-  })
   .get('/ussd', async (req, res) => {
     try {
       const client = await pool.connect()
@@ -46,7 +34,17 @@ express()
       res.send("Error " + err);
     }
   })
-  .post('/ussd', async (req, res) => {
+  .post('/ussd', (req, res) => {
+    console.log(req.headers)
+    console.log(req.body)
+    res.status(200).send({
+      "ussd-continue": {
+        "message": {
+          "encoding": "default",
+          "body": "looks good!"
+        }
+      }
+    })
     try {
       var description = JSON.stringify(req.body)
       const client = await pool.connect()
@@ -75,7 +73,7 @@ express()
     }
   })
   .post('/http', async (req, res) => {
-    if (req.header('Authorization') !== `Bearer ${process.env.APP_TOKEN}`) {
+    if (req.headers('Authorization') !== `Bearer ${process.env.APP_TOKEN}`) {
       let err = 'Invalid token provided\n'
       console.log(err)
       res.status(401).end(err)
